6caa9ca84374bb1fe66b92bd303830404ae3db56
Razzi Abuissa <razzi53@gmail.com>
2019-12-06 02:24:47 -0600

    Solution to part 2 for day 4

--- 04-password/password.clj
+++ 04-password/password.clj
@@ -3,13 +3,60 @@
 (defn adjacent-pairs [l]
   (map vector (butlast l) (rest l)))
 
-(adjacent-pairs [1 2 3])
+(defn adjacent-triplets [l]
+  (map vector (butlast (butlast l)) (butlast (rest l)) (rest (rest l))))
+
+;; (adjacent-triplets [1 2 3 4])
 
 (defn has-adjacent-digits? [digits]
-  (let [pairs (adjacent-pairs digits)]
-    (some true? (map (fn [[a b]] (= a b)) pairs))))
+  (let [triplets (adjacent-triplets digits)]
+    (some true? (map (fn [[a b c]]
+                       (and c
+                            (= a b)
+                            (not= b c))) triplets))))
 
+;; (first '())
+;; (defn group-start [digit remainder]
+;;   (let [f (first digits)]
+;;     (if (nil? f)
+;;       [digit]
+;;       (if (= digit f)
+;;         )))
+;;   (if ))
+;; (defn group-by-number [digits]
+;;   (let [f (first digits)]
+;;     (if (nil? f)
+;;       []
+;;       ()))
+;;   (take-while ))
+
+; good
 (has-adjacent-digits? [1 1 3])
+
+(defn has-adjacent-digits? [digits]
+  (let [pairs (adjacent-pairs digits)
+        same-pairs (map (fn [[a b]] (= a b)) pairs)
+        same-pair-pairs (adjacent-pairs same-pairs)
+        same-pairs-disqualified (map (fn [[a b]] (and a b)) same-pair-pairs)
+        left-aligned-disqualified (concat '(false) same-pairs-disqualified)
+        right-aligned-disqualified (concat same-pairs-disqualified '(false))
+        triplets (map vector same-pairs left-aligned-disqualified right-aligned-disqualified)
+        ;; interleaved (interleave same-pairs same-pairs-disqualified)
+        ;; value-and-disqualified (map vector same-pairs lined-up-disqualified)
+        ;; qualified-good (map (fn [[good disqualified]] (and good (not disqualified))) value-and-disqualified)
+        ]
+    ;; (prn same-pairs)
+    ;; (prn same-pairs-disqualified)
+    (some (fn [[good disq1 disq2]]
+            (and good (not disq1) (not disq2))) triplets)))
+;; (and (some true? same-pairs)
+;;      (not (some (fn [[a b]] (and a b)) same-pair-pairs)))
+; bad
+(has-adjacent-digits? [1 1 2 2 2])
+(has-adjacent-digits? [1 1 1 3 3 3])
+
+(has-adjacent-digits? [1 1 1])
+(has-adjacent-digits? [1 1 2 2 3 4 4 4])
 (has-adjacent-digits? [1 2 3])
 
 (defn digits-increase? [digits]
